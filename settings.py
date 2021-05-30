c2b60cccd6199bcc2886fc2b137bafaee313a50e
TimoJust <81025578+TimoJust@users.noreply.github.com>
2021-05-30 15:02:00 +0200

    Settings_Änderung

--- settings.py
+++ settings.py
@@ -1,8 +1,15 @@
+#Autor: Timo Just
+
+import pygame
 # Settings
-WIDTH = 992
-HEIGHT = 558
+WIDTH = 1280
+HEIGHT = 720
 FPS = 60
 WEISS = (255, 255, 255)
 SCHWARZ = (0, 0, 0)
 BLAU = (0, 0, 255)
 ROT = (255, 0, 0)
+SLATEGRAY_3 = (159,182,205)
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+position = (0,0)
+pygame.font.init()
