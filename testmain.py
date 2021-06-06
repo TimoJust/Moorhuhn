35be4492b3292f765c1b5afcfade488a5f848645
Chillery93 <81111382+Chillery93@users.noreply.github.com>
2021-06-06 15:31:20 +0200

    Delete testmain.py

--- testmain.py
+++
@@ -1,7 +0,0 @@
-from strategy_spielstufe_auswählen import*
-
-spielstufe_Startbildschirm.spielstufe_ausführen()
-spielstufe_Namenseingabe.spielstufe_ausführen()
-spielstufe_Heldenauswahl.spielstufe_ausführen()
-spielstufe_Levelauswahl.spielstufe_ausführen()
-spielstufe_Spielende.spielstufe_ausführen()
